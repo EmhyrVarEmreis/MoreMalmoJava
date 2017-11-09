f2c910c93cc5944e0025daa20552f4d83c3a8dda
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2017-11-09 15:17:29 +0100

    Init Example Run

---
+++ src/main/java/xyz/morecraft/dev/malmo/Main.java
@@ -0,0 +1,11 @@
+package xyz.morecraft.dev.malmo;
+
+import example.JavaExamples_run_mission;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JavaExamples_run_mission.main(args);
+    }
+
+}
